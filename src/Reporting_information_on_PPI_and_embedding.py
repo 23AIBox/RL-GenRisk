a5074f97714425a493730bdcc5d385bc435da5fd
dzLu1203 <82451879+dzLu1203@users.noreply.github.com>
2024-10-24 21:01:55 +0800

    Update Reporting_information_on_PPI_and_embedding.py

--- src/Reporting_information_on_PPI_and_embedding.py
+++ src/Reporting_information_on_PPI_and_embedding.py
@@ -255,3 +255,86 @@ for i in range(len(gene_ranking)):
 
 
 draw_2_box(lst_cosine_top20, lst_cosine_ALL,'Cosine Similarity', 'top 20', 'cosine_similarity.pdf', False)
+
+# The proportion of known risk genes in Top K genes
+def draw_top_k_proportion(lst_topK, method, num):
+
+    filename_1_intogen = 'sta_ccRCC_intogen.txt'
+    filename_1_ngc = 'sta_ccRCC_NGC.txt'
+    filename_1_all = 'sta_ccRCC_all.txt'
+
+    df_gold_genes_intogen = pd.read_csv(filename_1_intogen, header=None)
+    lst_gold_genes_intogen = df_gold_genes_intogen[0].tolist()
+    
+    df_gold_genes_ngc = pd.read_csv(filename_1_ngc, header=None)
+    lst_gold_genes_ngc = df_gold_genes_ngc[0].tolist()
+
+    df_gold_genes_all = pd.read_csv(filename_1_all, header=None)
+    lst_gold_genes_all = df_gold_genes_all[0].tolist()
+
+    print(len(lst_gold_genes_intogen))
+    print(len(lst_gold_genes_ngc))
+    print(len(lst_gold_genes_all))
+
+    gold_standards = {
+        'IntOGen': lst_gold_genes_intogen,
+        'NGC': lst_gold_genes_ngc,
+        'Merged': lst_gold_genes_all
+    }
+
+    topK_genes = lst_topK[:num]
+    overlap_matrix = []
+    k_lst = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+    dict_topk_acc = {}
+    dict_topk_acc['IntOGen'] = {}
+    dict_topk_acc['NGC'] = {}
+    dict_topk_acc['Merged'] = {}
+    
+    for k in k_lst:
+        dict_topk_acc['IntOGen'][k] = 0
+        dict_topk_acc['NGC'][k] = 0
+        dict_topk_acc['Merged'][k] = 0
+
+    for k in k_lst:
+        cnt_intogen = 0
+        cnt_ngc = 0
+        cnt_merged = 0
+        for i in range(k):
+            if top100_genes[i] in lst_gold_genes_intogen:
+                cnt_intogen+=1
+            if top100_genes[i] in lst_gold_genes_ngc:
+                cnt_ngc+=1
+            if top100_genes[i] in lst_gold_genes_all:
+                cnt_merged+=1
+        dict_topk_acc['IntOGen'][k] = cnt_intogen/k
+        dict_topk_acc['NGC'][k] = cnt_ngc/k
+        dict_topk_acc['Merged'][k] = cnt_merged/k
+        
+        data = dict_topk_acc
+    
+    bar_width = 0.25
+    index = np.array(list(data['IntOGen'].keys()))
+    indices = np.arange(len(index))
+    intogen_values = list(data['IntOGen'].values())
+    ngc_values = list(data['NGC'].values())
+    merged_values = list(data['Merged'].values())
+
+    plt.figure(figsize=(12, 6))
+    color_g = (194/255, 207/255, 162/255)
+    color_p = (164/255, 121/255, 158/255)
+    color_q = (94/255, 166/255, 156/255)
+    plt.bar(indices - bar_width, intogen_values, bar_width, label='IntOGen', color = color_p)
+    plt.bar(indices, ngc_values, bar_width, label='NGC',color = color_g)
+    plt.bar(indices + bar_width, merged_values, bar_width, label='Merged',color=color_q)
+    plt.xlabel('Top K Identified Risk Genes', fontsize=20)
+    plt.ylabel('Percentage', fontsize=20)
+    plt.xticks(indices, index, fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(frameon=False,fontsize=15)
+
+    plt.savefig("TopK_known_Proportion.pdf", bbox_inches='tight')
+    plt.show()
+
+
+    
+    
